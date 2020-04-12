2f743d76ca0568adefed6684e7b335658d6b62d0
wsyzxxxx <645658325@qq.com>
2020-04-12 18:10:28 +0800

    updatae server port

--- models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
+++ models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
@@ -2,6 +2,6 @@ package edu.duke651.wlt.models;
 
 public class ServerSetting {
     public static final String HOST = "willxxx.xin";
-    public static final int PORT = 6666;
+    public static final int PORT = 8080;
     public static final int PLAYER_NUM = 3;
 }
