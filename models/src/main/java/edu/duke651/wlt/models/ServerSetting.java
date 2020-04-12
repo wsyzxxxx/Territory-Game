f2f507e65b7259ff27c285a167befeaf981d7136
wsyzxxxx <645658325@qq.com>
2020-04-12 17:51:46 +0800

    modify server

--- models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
+++ models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
@@ -1,7 +1,7 @@
 package edu.duke651.wlt.models;
 
 public class ServerSetting {
-    public static final String HOST = "127.0.0.1";
+    public static final String HOST = "willxxx.xin";
     public static final int PORT = 6666;
     public static final int PLAYER_NUM = 3;
 }
