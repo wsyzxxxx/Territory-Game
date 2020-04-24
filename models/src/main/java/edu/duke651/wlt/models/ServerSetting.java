959cea115e2f2db7dae90895183e2ae4d2e4a52e
wsyzxxxx <645658325@qq.com>
2020-04-24 23:17:01 +0800

    for gradle test

--- models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
+++ models/src/main/java/edu/duke651/wlt/models/ServerSetting.java
@@ -24,14 +24,14 @@ public class ServerSetting {
     public static final int INIT_TECH_RESOURCE_GENERATE_LEVEL = 60;
     public static final int INIT_FOOD_RESOURCE_GENERATE_LEVEL_BASE = 5;
     public static final int INIT_FOOD_RESOURCE_GENERATE_LEVEL = 60;
-    public static final Map<Integer, String> PLAYER_TERRITORY_GROUP_MAP = new HashMap<>() {{
+    public static final Map<Integer, String> PLAYER_TERRITORY_GROUP_MAP = new HashMap<Integer, String>() {{
         put(2, "[[\"Narnia\", \"Midkemia\", \"Oz\", \"Gondor\", \"Mordor\"], [\"Elantris\", \"Scadrial\", \"Roshar\", \"Hogwarts\"]]");
         put(3, "[[\"Narnia\", \"Midkemia\", \"Oz\"], [\"Gondor\", \"Mordor\", \"Hogwarts\"], [\"Elantris\", \"Scadrial\", \"Roshar\"]]");
         put(4, "[[\"Narnia\", \"Elantris\"], [\"Midkemia\", \"Oz\", \"Scadrial\"], [\"Gondor\", \"Mordor\"], [\"Hogwarts\", \"Roshar\"]]");
         put(5, "[[\"Narnia\", \"Midkemia\"], [\"Oz\", \"Gondor\"], [\"Mordor\", \"Hogwarts\"], [\"Elantris\"], [\"Scadrial\", \"Roshar\"]]");
     }};
     public static final String[] COLOR_SET = {"#00a6ac", "#e0861a", "#7fb80e", "#aa2116", "#694d9f"};
-    public static final Map<String, Polyline> TERRITORY_POLY_MAP = new HashMap<>() {{
+    public static final Map<String, Polyline> TERRITORY_POLY_MAP = new HashMap<String, Polyline>() {{
         put("Elantris", new Polyline(  300,200,  400,190, 400,270, 370,350, 320,350, 260,250, 300,200));
         put("Roshar", new Polyline(  400,270,  370,350, 570,350, 580,290, 480,290, 400,270));
         put("Scadrial", new Polyline(  400,270,  400,230, 500,210, 540,250, 590,250, 580,290, 480,290, 400,270));
@@ -42,7 +42,7 @@ public class ServerSetting {
         put("Midkemia", new Polyline(  400,190,  400,230, 490,212, 590,100, 460,70, 400,190));
         put("Oz", new Polyline(  490,212,  500,210, 585,190, 590,100, 490,212));
     }};
-    public static final Map<String, Pair<Integer, Integer>> NAME_ON_MAP = new HashMap<>() {{
+    public static final Map<String, Pair<Integer, Integer>> NAME_ON_MAP = new HashMap<String, Pair<Integer, Integer>>() {{
         put("Elantris", new Pair<>(320, 250));
         put("Roshar", new Pair<>(430, 320));
         put("Scadrial", new Pair<>(450, 260));
@@ -62,7 +62,7 @@ public class ServerSetting {
     * @Author: Leo
     * @Date: 2020/4/21
     */
-    public static final Map<Integer, Integer> TECH_LEVEL_COST_MAP = new HashMap<>() {{
+    public static final Map<Integer, Integer> TECH_LEVEL_COST_MAP = new HashMap<Integer, Integer>() {{
         put(1, 0);
         put(2, 50);
         put(3, 75);
@@ -78,7 +78,7 @@ public class ServerSetting {
     * @Author: Leo
     * @Date: 2020/4/21
     */
-    public static final Map<Integer, UnitInfo> UNIT_LEVEL_COST_MAP = new HashMap<>() {{
+    public static final Map<Integer, UnitInfo> UNIT_LEVEL_COST_MAP = new HashMap<Integer, UnitInfo>() {{
         put(0, new UnitInfo(0, "Peasant", 0, 0));
         put(1, new UnitInfo(1, "Archer", 1, 3));
         put(2, new UnitInfo(2, "Swordsman", 3, 11));
